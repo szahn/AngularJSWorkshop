017ed66507ee70dee4052dbadddb49776c5793a2
Stuart Zahn <stuart.zahn@gmail.com>
2014-05-31 10:31:43 -0700

    Starting Angular JS workshop

---
+++ Lesson2/app.js
@@ -0,0 +1,11 @@
+function TweetCtrl($scope){
+	$scope.term = "angularjs";
+	$scope.doSearch = function(term){
+		//You could get the term from the scope, or as a function arg
+		//Method easier to unit test when accepting as an argument
+		//var term = $scope.term;
+		console.debug(term);
+
+		$scope.results = ["bob", "mike"];
+	}
+}
